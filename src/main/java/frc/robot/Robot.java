386636d5d17d30929570a567f05279cc6d8a349a
Awsomeworld304 <bucheckerr@gmail.com>
2025-02-09 16:07:31 -0500

    Cleanup robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,24 +4,15 @@
 
 package frc.robot;
 
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.networktables.PubSubOption;
-import edu.wpi.first.networktables.StringPublisher;
-import edu.wpi.first.networktables.StringTopic;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.CommandSwerveDrivetrain;
+
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private final RobotContainer m_robotContainer;
-  /*example 
-  NetworkTableInstance inst = NetworkTableInstance.getDefault();
-  StringTopic strTopic = inst.getStringTopic("/Elastic/CANColorValues");
-  StringPublisher strPub = strTopic.publish(PubSubOption.sendAll(true));
-  */
 
   public Robot() {
     m_robotContainer = new RobotContainer();
@@ -30,10 +21,6 @@ public class Robot extends TimedRobot {
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run(); 
-    /* example
-    String canandcolorData = new String("Proximity: " + 1 +  " Red Value: " + 1 + " Blue Value: " + 1 + " Green Value: " + 1);
-    strPub.accept(canandcolorData);
-    */
   }
 
   @Override
