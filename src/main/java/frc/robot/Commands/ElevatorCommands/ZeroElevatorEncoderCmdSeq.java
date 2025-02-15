0f64418acf232a4302eb09afdde57f060a0c0d92
BeanieBot <team3492@gmail.com>
2025-02-15 10:47:09 -0500

    commented partsNT

--- src/main/java/frc/robot/Commands/ElevatorCommands/ZeroElevatorEncoderCmdSeq.java
+++ src/main/java/frc/robot/Commands/ElevatorCommands/ZeroElevatorEncoderCmdSeq.java
@@ -24,7 +24,7 @@ public class ZeroElevatorEncoderCmdSeq extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    elevator2.zeroElevatorEncoders();
+    elevator2.zeroEncoders();
   }
 
   // Called once the command ends or is interrupted.
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,26 +4,28 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-public class Robot extends TimedRobot {
 
-  @Override
-  public void robotInit() {
-  }
+public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
+
+  //PARTsNT partsNT;
 
   private final RobotContainer m_robotContainer;
 
   public Robot() {
     m_robotContainer = new RobotContainer();
+  //  partsNT = new PARTsNT(this);
   }
 
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run(); 
+  //  partsNT.setDouble("Match Time", DriverStation.getMatchTime());
   }
 
   @Override
