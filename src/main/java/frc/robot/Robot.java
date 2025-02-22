ea2fd9e87534d42377fc71a247d49f9456272c5a
beaniebot <team3492@gmail.com>
2025-02-22 18:58:23 -0500

    algae doing things

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.util.PARTsLogger;
 import frc.robot.util.PARTsNT;
+import au.grapplerobotics.CanBridge;
 
 
 
@@ -24,6 +25,7 @@ public class Robot extends TimedRobot {
   private final RobotContainer m_robotContainer;
 
   public Robot() {
+    CanBridge.runTCP();
 
     partsNT = new PARTsNT(this);
     partsLogger = new PARTsLogger();
