b22722e27cecf4131a4c5bbe461cd34fd7a8e137
Brandon <brandon@bduke.dev>
2025-02-23 12:54:18 -0500

    remove lib

--- src/main/java/frc/robot/subsystems/Coral.java
+++ src/main/java/frc/robot/subsystems/Coral.java
@@ -18,7 +18,6 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants;
 import frc.robot.subsystems.Candle.CandleState;
