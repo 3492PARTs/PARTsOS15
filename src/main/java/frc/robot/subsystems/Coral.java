ef7c223692291f290494df99a7e965049e84b948
Awsomeworld304 <bucheckerr@gmail.com>
2025-03-01 11:38:17 -0500

    Added commandfactory names to subsys commands

--- src/main/java/frc/robot/subsystems/Coral.java
+++ src/main/java/frc/robot/subsystems/Coral.java
@@ -198,71 +198,89 @@ public class Coral extends PARTsSubsystem {
   }
 
   public Command intake() {
-    return this.runOnce(() -> {
-      mPeriodicIO.speed_diff = 0.0;
-      mPeriodicIO.rpm = Constants.Coral.kIntakeSpeed;
-      mPeriodicIO.state = IntakeState.INTAKE;
-    });
+    return super.commandFactory("coralIntake",
+      this.runOnce(() -> {
+        mPeriodicIO.speed_diff = 0.0;
+        mPeriodicIO.rpm = Constants.Coral.kIntakeSpeed;
+        mPeriodicIO.state = IntakeState.INTAKE;
+      }
+    ));
   }
 
   public Command reverse() {
-    return this.runOnce(() -> {
-      mPeriodicIO.speed_diff = 0.0;
-      mPeriodicIO.rpm = Constants.Coral.kReverseSpeed;
-      mPeriodicIO.state = IntakeState.REVERSE;
-    });
+    return super.commandFactory("coralReverse",
+      this.runOnce(() -> {
+        mPeriodicIO.speed_diff = 0.0;
+        mPeriodicIO.rpm = Constants.Coral.kReverseSpeed;
+        mPeriodicIO.state = IntakeState.REVERSE;
+      }
+    ));
   }
 
   public Command index() {
-    return this.runOnce(() -> {
-      mPeriodicIO.speed_diff = 0.0;
-      mPeriodicIO.rpm = Constants.Coral.kIndexSpeed;
-      mPeriodicIO.state = IntakeState.INDEX;
-    });
+    return super.commandFactory("coralIndex",
+      this.runOnce(() -> {
+        mPeriodicIO.speed_diff = 0.0;
+        mPeriodicIO.rpm = Constants.Coral.kIndexSpeed;
+        mPeriodicIO.state = IntakeState.INDEX;
+      }
+    ));
   }
 
   public Command scoreL1() {
-    return this.runOnce(() -> {
-      mPeriodicIO.speed_diff = Constants.Coral.kSpeedDifference;
-      mPeriodicIO.rpm = Constants.Coral.kL1Speed;
-      mPeriodicIO.state = IntakeState.SCORE;
-    });
+    return super.commandFactory("coralScoreL1",
+      this.runOnce(() -> {
+        mPeriodicIO.speed_diff = Constants.Coral.kSpeedDifference;
+        mPeriodicIO.rpm = Constants.Coral.kL1Speed;
+        mPeriodicIO.state = IntakeState.SCORE;
+      }
+    ));
   }
 
+  /**
+   * Score L2 - L4
+   * @return The score command.
+   */
   public Command scoreL24() {
-    return this.runOnce(() -> {
-      mPeriodicIO.speed_diff = 0.0;
-      mPeriodicIO.rpm = Constants.Coral.kL24Speed;
-      mPeriodicIO.state = IntakeState.SCORE;
-    });
+    return super.commandFactory("coralScoreL24",
+      this.runOnce(() -> {
+        mPeriodicIO.speed_diff = 0.0;
+        mPeriodicIO.rpm = Constants.Coral.kL24Speed;
+        mPeriodicIO.state = IntakeState.SCORE;
+      }
+    ));
   }
 
   public Command stopCoral() {
-    return this.runOnce(() -> {
-      mPeriodicIO.rpm = 0.0;
-      mPeriodicIO.speed_diff = 0.0;
-      mPeriodicIO.state = IntakeState.NONE;
-    });
+    return super.commandFactory("coralStop",
+      this.runOnce(() -> {
+        mPeriodicIO.rpm = 0.0;
+        mPeriodicIO.speed_diff = 0.0;
+        mPeriodicIO.state = IntakeState.NONE;
+      }
+    ));
 
   }
 
   public Command score() {
-    return this.runOnce(() -> {
-      candle.addState(CandleState.SCORING);
-      switch (elevator.getState()) {
-        case STOW:
-          scoreL1().schedule();
-          break;
-        default:
-          scoreL24().schedule();
-          break;
+    return super.commandFactory("coralScore",
+      this.runOnce(() -> {
+        candle.addState(CandleState.SCORING);
+        switch (elevator.getState()) {
+          case STOW:
+            scoreL1().schedule();
+            break;
+          default:
+            scoreL24().schedule();
+            break;
+        }
       }
-    });
+    ));
   }
 
   public Command scoreCommand() {
-    return score().andThen(new WaitUntilCommand(() -> mPeriodicIO.state == IntakeState.NONE))
-        .andThen(elevator.goToElevatorStow());
+    return super.commandFactory("coralScoreCmd", score().andThen(new WaitUntilCommand(() -> mPeriodicIO.state == IntakeState.NONE))
+    .andThen(elevator.goToElevatorStow()));
   }
 
   /*---------------------------------- Custom Private Functions ---------------------------------*/
