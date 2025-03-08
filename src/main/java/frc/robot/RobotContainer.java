0f79b5659fa0ab15bd90bc263bde752fa01ee3d6
BeanieBot <team3492@gmail.com>
2025-03-08 14:37:56 -0500

    changed a button

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -175,7 +175,7 @@ public class RobotContainer {
                 // Run SysId routines when holding back/start and X/Y.
                 // Note that each routine should be run exactly once in a single log.
                 /*
-                
+
                  * driveController.back().and(driveController.y()).whileTrue(drivetrain.
                  * sysIdDynamic(Direction.kForward));
                  * driveController.back().and(driveController.x()).whileTrue(drivetrain.
@@ -219,7 +219,7 @@ public class RobotContainer {
                 buttonBoxController.enginestartTrigger().onTrue(coral.score());
 
                 //---------------------  Stow --------------------//
-                buttonBoxController.escTrigger().onTrue(elevator.goToElevatorStow());
+                buttonBoxController.negative1Trigger().onTrue(elevator.goToElevatorStow());
 
                 //*---------------------------------------------------- *//
                 //* ---------------Right Reef Pole Controls ----------- *//
