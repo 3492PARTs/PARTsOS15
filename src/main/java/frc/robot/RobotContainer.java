a8acace9df0007428caaf9aa5eb4a0d73d18dc17
Awsomeworld304 <bucheckerr@gmail.com>
2025-03-15 19:21:39 -0400

    Change controllers to PARTsCommandControllers

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -35,10 +35,12 @@ import frc.robot.subsystems.sysid.ElevatorSysId;
 import frc.robot.subsystems.testing.Buttonbox;
 import frc.robot.util.IPARTsSubsystem;
 import frc.robot.util.PARTsButtonBoxController;
+import frc.robot.util.PARTsCommandController;
 import frc.robot.util.PARTsDashboard;
 import frc.robot.util.PARTsNT;
 import frc.robot.util.PARTsSubsystem;
 import frc.robot.util.PARTsUnit;
+import frc.robot.util.PARTsController.ControllerType;
 import frc.robot.util.PARTsUnit.PARTsUnitType;
 import frc.robot.subsystems.Coral;
 import frc.robot.subsystems.Elevator;
@@ -62,8 +64,8 @@ public class RobotContainer {
 
         private final Telemetry telemetryLogger = new Telemetry(MaxSpeed);
 
-        private final CommandXboxController driveController = new CommandXboxController(0);
-        private final CommandXboxController operatorController = new CommandXboxController(1);
+        private final PARTsCommandController driveController = new PARTsCommandController(0, ControllerType.XBOX);
+        private final PARTsCommandController operatorController = new PARTsCommandController(1, ControllerType.XBOX);
         private final PARTsButtonBoxController buttonBoxController = new PARTsButtonBoxController(2);
 
         private boolean elevatorManualControl = false;
