65fcb44ac7a71cfc774fbec9943b39fa8c2f5015
megna10 <megp_15@outlook.com>
2025-04-17 22:55:27 -0400

    Increase L1 speed and refined autos

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -153,7 +153,7 @@ public final class Constants {
 
     public static final double kIntakeSpeed = -0.15;//-0.3;
     public static final double kReverseSpeed = 0.6;
-    public static final double kL1Speed = -0.2;
+    public static final double kL1Speed = -0.22;
     public static final double kL23Speed = -0.25;
     public static final double kL4Speed = -.2;
     public static final double kIndexSpeed = -0.135;
